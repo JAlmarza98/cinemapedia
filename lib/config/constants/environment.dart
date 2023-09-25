16aecd16fd220681a72ea3210619e5a18cdee8b6
JAlmarza98 <j.almarza98@gmail.com>
2023-09-25 20:54:42 +0200

    🔐 Environments actualizados

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -3,4 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Environment {
   static String theMovieDbKey =
       dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api key';
+  static String theMovieDbUrl =
+      dotenv.env['THE_MOVIEDB_API_URL'] ?? 'No hay api URL';
 }
